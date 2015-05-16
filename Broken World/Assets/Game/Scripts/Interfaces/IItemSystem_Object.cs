72dd3fc6feefa82e7e52853eda5461d58eb76208
Jonathan Bark <jonathanbark85@gmail.com>
2015-05-16 12:48:59 +0200

    Folder Hierark

---
+++ Broken World/Assets/Game/Scripts/Interfaces/IItemSystem_Object.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IItemSystem_Object{
+	string Name{ get; set;}
+	int ObjectValue{ get; set;}
+	Sprite Icon{ get; set;}
+	int Weight{ get; set;}
+}
---
+++ Broken World/Assets/Game/Scripts/Interfaces/IItemSystem_Quality.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IItemSystem_Quality{
+	string Name{ get; set;}
+	Color ObjectColor{ get; set;}
+	Sprite Icon{ get; set;}
+}
