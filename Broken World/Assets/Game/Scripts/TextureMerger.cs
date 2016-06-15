d9c82c65e1298c1ce6030cd0d8a2c46e8d99e103
Jonathan Bark <jonathanbark85@gmail.com>
2016-06-15 03:31:59 +0200

    Starting to look into shaders and stuff :S

---
+++ Broken World/Assets/Game/Scripts/TextureMerger.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TextureMerger: MonoBehaviour {
+
+    public GameObject textureTestObject;
+    public Texture2D[] TextureLayers;
+    public Texture2D MergedTexture;
+    public int TileWidth = 32;
+    public int TileHeight = 32;
+
+    public void OnStart()
+    {
+        MergedTexture = new Texture2D(1024,1024);
+        if (TextureLayers.Length > 0)
+        {
+            Merge();
+        }
+
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+            Merge();
+
+    }
+
+
+
+    public void Merge()
+    { 
+        
+
+        //Get layer count
+        int layerCount = TextureLayers.Length;
+       
+        if(layerCount >0)
+        {
+            //MergedTexture = new Texture2D(TextureLayers[0].width, TextureLayers[0].height);
+            
+            ////Get pixel count 
+            //int pixelCount = TextureLayers[0].GetPixels().Length;
+            //Color[] mergedTexturePixels = new Color[pixelCount];
+
+            //Color[,] layersPixels = new Color[layerCount, pixelCount];
+
+            ////Get pixels
+            //for (int x = 0; x < layerCount; x++)
+            //{ 
+            //    //layersPixels.SetValue(TextureLayers[x].GetPixels(), x);
+            //    for (int y = 0; y < pixelCount; y++)
+            //    {
+
+            //            mergedTexturePixels[y] = layersPixels[x, y];
+                    
+            //    }
+
+
+
+            //}
+
+            //MergedTexture.SetPixels(mergedTexturePixels);
+
+            MergedTexture = CombineTextures(TextureLayers[1], TextureLayers[0]);
+
+
+            MergedTexture.Apply();
+            Texture2D texture = textureTestObject.GetComponent<Texture2D>();
+            texture = MergedTexture;
+        }
+    }
+
+
+    /// <summary>
+    /// Slår ihop två texturer, funkar som det ser ut nu endast två texturer, vilket kan vara bristfälligt
+    /// </summary>
+    /// <param name="Background"></param>
+    /// <param name="Overlay"></param>
+    /// <returns></returns>
+    public Texture2D CombineTextures (Texture2D Background, Texture2D Overlay) 
+    {
+        var newPixels = Background.GetPixels();
+        var pixelsOverlay = Overlay.GetPixels();
+
+        //Create a new Texture object
+        var newTexture = new Texture2D(Background.width, Background.height);
+
+        for (int x = 0; x < newPixels.Length; x++)
+        {
+            if (pixelsOverlay[x].a == 1)
+                 newPixels[x] = pixelsOverlay[x];
+        }
+
+        newTexture.SetPixels(newPixels);
+        newTexture.Apply();
+
+        return newTexture;
+     }
+}
---
+++ Broken World/Assets/Game/Shaders/JoppeTest.shader
@@ -0,0 +1,108 @@
+﻿// Default Sprite Shader combined with an indexed color texture (Palette)
+Shader "Custome/JoppeTest"
+{
+	Properties
+	{
+		[PerRendererData] _MainTex ("Sprite Texture", 2D) = "white" {}
+		_NormalMap("Normal Map", 2D) = "white" {}
+		_Palette ("Palette Texture", 2D) = "white" {}
+		_Tint ("Tint", Color) = (1,1,1,1)
+		[MaterialToggle] PixelSnap ("Pixel snap", Float) = 0
+		
+	}
+
+	SubShader
+	{
+		Tags
+		{ 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		Fog { Mode Off }
+		Blend One OneMinusSrcAlpha
+
+		Pass
+		{
+		CGPROGRAM
+
+			//Run Shader programs
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile DUMMY PIXELSNAP_ON
+			#include "UnityCG.cginc"
+			
+			//Define Structs used by the shader programs
+			struct appdata
+			{
+				float4 vertex   : POSITION;
+				float3 normal : NORMAL;
+				float4 color    : COLOR;
+				float2 texcoord : TEXCOORD0;
+				float4 tangent : TANGENT;
+			};
+
+			struct v2f
+			{
+				float4 vertex   : SV_POSITION;
+				float3 normal : NORMAL;
+				fixed4 color    : COLOR;
+				half2 texcoord  : TEXCOORD0;
+				float4 tangent : TANGENT;
+			};
+			
+			fixed4 _Tint;
+			sampler2D _MainTex;
+			sampler2D _Palette;
+			sampler2D _NormalMap;
+			float4 _Palette_ST; 
+
+			v2f vert(appdata IN)
+			{
+				v2f OUT;
+				OUT.vertex = mul(UNITY_MATRIX_MVP, IN.vertex);
+				OUT.texcoord = IN.texcoord;
+				// Removed the tinting here, since it would tint the palette map
+				//OUT.color = IN.color;
+
+
+				//OUT.color.xyz = IN.normal;
+				float3 bitangent = cross( IN.normal, IN.tangent.xyz ) * IN.tangent.w;
+				OUT.color.xyz = bitangent * 0.5 + 0.5;
+				OUT.color.w = 1.0;
+
+				#ifdef PIXELSNAP_ON
+				OUT.vertex = UnityPixelSnap (OUT.vertex);
+				#endif
+
+				return OUT;
+			}
+
+			fixed4 frag(v2f IN) : SV_Target
+			{
+				fixed4 paletteMapColor = tex2D(_MainTex, IN.texcoord) * IN.color;
+				
+				// The alpha channel of the palette map points to UVs in the palette key.
+				float paletteX = paletteMapColor.a;
+				float2 paletteUV = float2(paletteX, 0.0f);
+				// Get the palette's UV accounting for texture tiling and offset
+				float2 paletteUVTransformed = TRANSFORM_TEX(paletteUV, _Palette);
+				
+				// Get the color from the palette key
+				fixed4 outColor = tex2D(_Palette, paletteUVTransformed);
+				
+				// Apply the tint to the final color
+				outColor *= _Tint;
+				return outColor;
+			}
+		ENDCG
+		}
+
+	}
+}
